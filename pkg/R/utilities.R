5fe78665e4b5fe0cf0705e4d6bac608075793adc
jfox (Professor Emeritus ) <jfox@mcmaster.ca>
2008-12-04 23:23:08 +0000

    version 1.4-6. John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdr@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/utilities.R
+++ pkg/R/utilities.R
@@ -1,4 +1,4 @@
-# last modified 22 October 2008 by J. Fox + slight changes 12 Aug 04 by Ph. Grosjean
+# last modified 3 December 2008 by J. Fox + slight changes 12 Aug 04 by Ph. Grosjean
 
 # utility functions
 
@@ -18,6 +18,13 @@ listLinearModels <- function(envir=.GlobalEnv, ...) {
 #        function(.x) "lm" == (class(eval(parse(text=.x), envir=envir))[1]))]
         function(.x) "lm" == (class(get(.x, envir=envir))[1]))]
     }
+	
+listAOVModels <- function(envir=.GlobalEnv, ...) {
+	objects <- ls(envir=envir, ...)
+	if (length(objects) == 0) NULL
+	else objects[sapply(objects,
+				function(.x) "aov" == (class(get(.x, envir=envir))[1]))]
+}
 
 listGeneralizedLinearModels <- function(envir=.GlobalEnv, ...) {
     objects <- ls(envir=envir, ...)
@@ -167,7 +174,7 @@ listNumeric <- function(dataSet=ActiveDataSet()) {
 trim.blanks <- function(text){
     gsub("^\ *", "", gsub("\ *$", "", text))
     }
-
+	
 is.valid.name <- function(x){
     length(x) == 1 && is.character(x) && x == make.names(x)
     }
@@ -1795,10 +1802,14 @@ modelsP <- function(n=1) activeDataSetP() && length(listAllModels()) >= n
 activeModelP <- function() !is.null(ActiveModel())
 
 #lmP <- function() activeModelP() && eval(parse(text=paste("class(", ActiveModel(), ")[1] == 'lm'")))
-lmP <- function() activeModelP() && class(get(ActiveModel()))[1] == 'lm'
+lmP <- function() activeModelP() && any(class(get(ActiveModel()))[1] == c('lm', 'aov'))
 
 #glmP <- function() activeModelP() && eval(parse(text=paste("class(", ActiveModel(), ")[1] == 'glm'")))
 glmP <- function() activeModelP() && class(get(ActiveModel()))[1] == 'glm'
+
+polrP <- function() activeModelP() && class(get(ActiveModel()))[1] == 'polr'
+
+multinomP <- function() activeModelP() && class(get(ActiveModel()))[1] == 'multinom'
 
 hclustSolutionsP <- function() length(listHclustSolutions()) > 0
 
@@ -2024,7 +2035,7 @@ is.model <- function(object) {
 if (!(as.character(tcl("info", "tclversion")) >= "8.5" && getRversion() >= "2.7.0")){
     buttonRcmdr <- tkbutton
     labelRcmdr <- tklabel
-    ttkentry <- tkentry
+    ttkentry <- function(parent, ...) tkentry(parent, ...)
     ttkframe <- tkframe
     ttkradiobutton <- tkradiobutton
     ttkscrollbar <- function(...) tkscrollbar(..., repeatinterval=5)
@@ -2032,7 +2043,11 @@ if (!(as.character(tcl("info", "tclversion")) >= "8.5" && getRversion() >= "2.7.
     buttonRcmdr <- function(..., borderwidth, fg, foreground, relief) ttkbutton(...)
     labelRcmdr <- function(..., fg)
         if(missing(fg)) ttklabel(...) else ttklabel(..., foreground=fg)
-    } 
+    }
+	
+# the following function alters the default behaviour of tclvalue() by trimming leading and trailing blanks
+
+tclvalue <- function(x) trim.blanks(tcltk::tclvalue(x))
 	
 # the following function returns the number of observations for a statistical model
 
@@ -2040,4 +2055,40 @@ nobs <- function(model){
 	fitted <- na.omit(fitted(model))
 	if (is.matrix(fitted)) nrow(fitted) else length(fitted)
 	}
+	
+# the following function slips a character string at blanks and commas according to width
 
+splitCmd <- function(cmd, width=getOption("width") - 2, at="[ ,]"){
+	if (nchar(cmd) <= width) return(cmd)
+	where <- gregexpr(at, cmd)[[1]]
+	if (where[1] < 0) return(cmd)
+	singleQuotes <- gregexpr("'", cmd)[[1]]
+	doubleQuotes <- gregexpr('"', cmd)[[1]]
+	comment <- regexpr("#", cmd)
+	if (singleQuotes[1] > 0){
+		nquotes <- length(singleQuotes)
+		if (nquotes %% 2 != 0) stop("unbalanced quotes")
+		for (left in seq(1, nquotes, 2)){
+			where[(where > singleQuotes[left]) & (where < singleQuotes[left + 1])] <- NA
+		}
+		where <- na.omit(where)
+	}  
+	if (doubleQuotes[1] > 0){
+		nquotes <- length(doubleQuotes)
+		if (nquotes %% 2 != 0) stop("unbalanced quotes")
+		for (left in seq(1, nquotes, 2)){
+			where[(where > doubleQuotes[left]) & (where < doubleQuotes[left + 1])] <- NA
+		}
+		where <- na.omit(where)
+	}
+	if (comment > 0){
+		where[where > comment] <- NA
+		where <- na.omit(where)
+	}
+	if (length(where) == 0) return(cmd)
+	where2 <- where[where <= width]
+	where2 <- if (length(where2) == 0) where[1]
+		else where2[length(where2)]
+	paste(substr(cmd, 1, where2), "\n", 
+		Recall(substr(cmd, where2 + 1, nchar(cmd)), width, at), sep="")
+} 
