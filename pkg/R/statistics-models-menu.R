9e926080e95c0a8e961148044e04e567b0735a07
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-07 17:22:29 +0000

    polr dialog memory. John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdr@147 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/statistics-models-menu.R
+++ pkg/R/statistics-models-menu.R
@@ -1,6 +1,6 @@
 # Statistics Menu dialogs
 
-# last modified 2011-11-27 by J. Fox
+# last modified 2011-12-07 by J. Fox
 
     # Models menu
 
@@ -365,90 +365,93 @@ generalizedLinearModel <- function(){
     dialogSuffix(rows=7, columns=1, focus=lhsEntry, preventDoubleClick=TRUE)
     }
 
-ordinalRegressionModel <- function(){
-    Library("MASS")
-    initializeDialog(title=gettextRcmdr("Ordinal Regression Model"))
-    .activeModel <- ActiveModel()
-    .activeDataSet <- ActiveDataSet()
-    currentModel <- if (!is.null(.activeModel))
-        class(get(.activeModel, envir=.GlobalEnv))[1] == "polr"
+	ordinalRegressionModel <- function(){
+		defaults <- list(initial.type="logistic")
+		dialog.values <- getDialog("ordinalRegressionModel", defaults)
+		Library("MASS")
+		initializeDialog(title=gettextRcmdr("Ordinal Regression Model"))
+		.activeModel <- ActiveModel()
+		.activeDataSet <- ActiveDataSet()
+		currentModel <- if (!is.null(.activeModel))
+					class(get(.activeModel, envir=.GlobalEnv))[1] == "polr"
 #        eval(parse(text=paste("class(", .activeModel, ")[1] == 'polr'", sep="")),
 #            envir=.GlobalEnv)
-        else FALSE
-    if (currentModel) {
-        currentFields <- formulaFields(get(.activeModel, envir=.GlobalEnv))
+				else FALSE
+		if (currentModel) {
+			currentFields <- formulaFields(get(.activeModel, envir=.GlobalEnv))
 #        currentFields <- formulaFields(eval(parse(text=.activeModel),
 #            envir=.GlobalEnv))
-        if (currentFields$data != .activeDataSet) currentModel <- FALSE
-        }
-    UpdateModelNumber()
-    modelName <- tclVar(paste("OrdRegModel.", getRcmdr("modelNumber"), sep=""))
-    modelFrame <- tkframe(top)
-    model <- ttkentry(modelFrame, width="20", textvariable=modelName)
-    radioButtons(name="modelType",
-        buttons=c("logistic", "probit"),
-        labels=gettextRcmdr(c("Proportional-odds logit", "Ordered probit")),
-        title=gettextRcmdr("Type of Model"))
-    onOK <- function(){
-        modelValue <- trim.blanks(tclvalue(modelName))
-        closeDialog()
-        if (!is.valid.name(modelValue)){
-            errorCondition(recall=proportionalOddsModel, message=sprintf(gettextRcmdr('"%s" is not a valid name.'), modelValue), model=TRUE)
-            return()
-            }
-        subset <- tclvalue(subsetVariable)
-        if (trim.blanks(subset) == gettextRcmdr("<all valid cases>") || trim.blanks(subset) == ""){
-            subset <- ""
-            putRcmdr("modelWithSubset", FALSE)
-            }
-        else{
-            subset <- paste(", subset=", subset, sep="")
-            putRcmdr("modelWithSubset", TRUE)
-            }
-        check.empty <- gsub(" ", "", tclvalue(lhsVariable))
-        if ("" == check.empty) {
-            errorCondition(recall=proportionalOddsModel, message=gettextRcmdr("Left-hand side of model empty."), model=TRUE)
-            return()
-            }
-        check.empty <- gsub(" ", "", tclvalue(rhsVariable))
-        if ("" == check.empty) {
-            errorCondition(recall=proportionalOddsModel, message=gettextRcmdr("Right-hand side of model empty."), model=TRUE)
-            return()
-            }
-        if (!is.factor(eval(parse(text=tclvalue(lhsVariable)), envir=get(.activeDataSet, envir=.GlobalEnv)))){
+			if (currentFields$data != .activeDataSet) currentModel <- FALSE
+		}
+		UpdateModelNumber()
+		modelName <- tclVar(paste("OrdRegModel.", getRcmdr("modelNumber"), sep=""))
+		modelFrame <- tkframe(top)
+		model <- ttkentry(modelFrame, width="20", textvariable=modelName)
+		radioButtons(name="modelType",
+				buttons=c("logistic", "probit"), initialValue=dialog.values$initial.type,
+				labels=gettextRcmdr(c("Proportional-odds logit", "Ordered probit")),
+				title=gettextRcmdr("Type of Model"))
+		onOK <- function(){
+			modelValue <- trim.blanks(tclvalue(modelName))
+			closeDialog()
+			if (!is.valid.name(modelValue)){
+				errorCondition(recall=proportionalOddsModel, message=sprintf(gettextRcmdr('"%s" is not a valid name.'), modelValue), model=TRUE)
+				return()
+			}
+			subset <- tclvalue(subsetVariable)
+			if (trim.blanks(subset) == gettextRcmdr("<all valid cases>") || trim.blanks(subset) == ""){
+				subset <- ""
+				putRcmdr("modelWithSubset", FALSE)
+			}
+			else{
+				subset <- paste(", subset=", subset, sep="")
+				putRcmdr("modelWithSubset", TRUE)
+			}
+			check.empty <- gsub(" ", "", tclvalue(lhsVariable))
+			if ("" == check.empty) {
+				errorCondition(recall=proportionalOddsModel, message=gettextRcmdr("Left-hand side of model empty."), model=TRUE)
+				return()
+			}
+			check.empty <- gsub(" ", "", tclvalue(rhsVariable))
+			if ("" == check.empty) {
+				errorCondition(recall=proportionalOddsModel, message=gettextRcmdr("Right-hand side of model empty."), model=TRUE)
+				return()
+			}
+			if (!is.factor(eval(parse(text=tclvalue(lhsVariable)), envir=get(.activeDataSet, envir=.GlobalEnv)))){
 #        if (!is.factor(eval(parse(text=tclvalue(lhsVariable)), envir=eval(parse(text=.activeDataSet), envir=.GlobalEnv)))){
-            errorCondition(recall=proportionalOddsModel, message=gettextRcmdr("Response variable must be a factor"))
-            return()
-            }
-        if (is.element(modelValue, listProportionalOddsModels())) {
-            if ("no" == tclvalue(checkReplace(modelValue, type=gettextRcmdr("Model")))){
-                UpdateModelNumber(-1)
-                proportionalOddsModel()
-                return()
-                }
-            }
-        formula <- paste(tclvalue(lhsVariable), tclvalue(rhsVariable), sep=" ~ ")
-        command <- paste("polr(", formula, ', method="', tclvalue(modelTypeVariable),
-            '", data=', .activeDataSet, subset, ", Hess=TRUE)", sep="")
-        logger(paste(modelValue, " <- ", command, sep=""))
-        assign(modelValue, justDoIt(command), envir=.GlobalEnv)
-        doItAndPrint(paste("summary(", modelValue, ")", sep=""))
-        activeModel(modelValue)
-        tkfocus(CommanderWindow())
-        }
-    OKCancelHelp(helpSubject="polr", model=TRUE)
-    tkgrid(labelRcmdr(modelFrame, text=gettextRcmdr("Enter name for model:")), model, sticky="w")
-    tkgrid(modelFrame, sticky="w")
-    modelFormula()
-    subsetBox(model=TRUE)
-    tkgrid(getFrame(xBox), sticky="w")
-    tkgrid(outerOperatorsFrame, sticky="w")
-    tkgrid(formulaFrame, sticky="w")
-    tkgrid(subsetFrame, sticky="w")
-    tkgrid(modelTypeFrame, sticky="w")
-    tkgrid(buttonsFrame, sticky="w")
-    dialogSuffix(rows=7, columns=1, focus=lhsEntry, preventDoubleClick=TRUE)
-    }
+				errorCondition(recall=proportionalOddsModel, message=gettextRcmdr("Response variable must be a factor"))
+				return()
+			}
+			if (is.element(modelValue, listProportionalOddsModels())) {
+				if ("no" == tclvalue(checkReplace(modelValue, type=gettextRcmdr("Model")))){
+					UpdateModelNumber(-1)
+					proportionalOddsModel()
+					return()
+				}
+			}
+			putDialog("ordinalRegressionModel", list(initial.type = tclvalue(modelTypeVariable)))
+			formula <- paste(tclvalue(lhsVariable), tclvalue(rhsVariable), sep=" ~ ")
+			command <- paste("polr(", formula, ', method="', tclvalue(modelTypeVariable),
+					'", data=', .activeDataSet, subset, ", Hess=TRUE)", sep="")
+			logger(paste(modelValue, " <- ", command, sep=""))
+			assign(modelValue, justDoIt(command), envir=.GlobalEnv)
+			doItAndPrint(paste("summary(", modelValue, ")", sep=""))
+			activeModel(modelValue)
+			tkfocus(CommanderWindow())
+		}
+		OKCancelHelp(helpSubject="polr", model=TRUE, reset = "ordinalRegressionModel")
+		tkgrid(labelRcmdr(modelFrame, text=gettextRcmdr("Enter name for model:")), model, sticky="w")
+		tkgrid(modelFrame, sticky="w")
+		modelFormula()
+		subsetBox(model=TRUE)
+		tkgrid(getFrame(xBox), sticky="w")
+		tkgrid(outerOperatorsFrame, sticky="w")
+		tkgrid(formulaFrame, sticky="w")
+		tkgrid(subsetFrame, sticky="w")
+		tkgrid(modelTypeFrame, sticky="w")
+		tkgrid(buttonsFrame, sticky="w")
+		dialogSuffix(rows=7, columns=1, focus=lhsEntry, preventDoubleClick=TRUE)
+	}
 
 multinomialLogitModel <- function(){
     Library("nnet")
