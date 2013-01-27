8b9296fc991534266c5e2058960609516bab631d
jfox (Professor Emeritus ) <jfox@mcmaster.ca>
2013-01-27 20:28:38 +0000

    R logo fix. John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdr@275 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/utilities.R
+++ pkg/R/utilities.R
@@ -1,4 +1,4 @@
-# last modified 2013-01-22 by J. Fox
+# last modified 2013-01-27 by J. Fox
 #  applied patch to improve window behaviour supplied by Milan Bouchet-Valat 2011-09-22
 #  slight changes 12 Aug 04 by Ph. Grosjean
 
@@ -974,10 +974,11 @@ initializeDialog <- defmacro(window=top, title="", offset=10, preventCrisp=FALSE
 			position <- if (any(position < 0)) "-50+50"
 					else paste("+", paste(offset + position, collapse="+"), sep="")
 			tkwm.geometry(window, position)
-		    if (.Platform$OS.type == "windows") {
-		        tkwm.iconify(window)
-		        tkwm.iconbitmap(window, system.file("etc", "R-logo.ico", package="Rcmdr"))
-		    }
+		    tcl("wm", "iconphoto", window, "::image::RlogoIcon")
+# 		    if (.Platform$OS.type == "windows") {
+# 		        tkwm.iconify(window)
+# 		        tkwm.iconbitmap(window, system.file("etc", "R-logo.ico", package="Rcmdr"))
+# 		    }
 		    tkwm.transient(window, CommanderWindow())
 		}
 )
