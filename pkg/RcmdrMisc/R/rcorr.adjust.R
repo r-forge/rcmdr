d30789c4f704738788381496f468c6df71f34a59
jfox (Professor Emeritus ) <jfox@mcmaster.ca>
2014-09-22 19:21:48 +0000

    make sure that .000 prints as <.001 in rcorr.adjust(). John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdr@582 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RcmdrMisc/R/rcorr.adjust.R
+++ pkg/RcmdrMisc/R/rcorr.adjust.R
@@ -4,17 +4,19 @@
 
 # last modified 2014-09-04 by J. Fox
 
-rcorr.adjust <- function(x, type=c("pearson", "spearman"), 
-    use=c("complete.obs", "pairwise.complete.obs")){
-    opt <- options(scipen=5)
+rcorr.adjust <- function (x, type = c("pearson", "spearman"), 
+            use = c("complete.obs", "pairwise.complete.obs")) {
+    opt <- options(scipen = 5)
     on.exit(options(opt))
     type <- match.arg(type)
     use <- match.arg(use)
-    x <- if (use == "complete.obs") as.matrix(na.omit(x)) else as.matrix(x)
-    R <- rcorr(x, type=type)
+    x <- if (use == "complete.obs") 
+      as.matrix(na.omit(x))
+    else as.matrix(x)
+    R <- rcorr(x, type = type)
     P <- P.unadj <- R$P
     p <- P[lower.tri(P)]
-    adj.p <- p.adjust(p, method="holm")
+    adj.p <- p.adjust(p, method = "holm")
     P[lower.tri(P)] <- adj.p
     P[upper.tri(P)] <- 0
     P <- P + t(P)
@@ -22,14 +24,16 @@ rcorr.adjust <- function(x, type=c("pearson", "spearman"),
     P <- format(round(P, 4))
     diag(P) <- ""
     P[c(grep("0.0000", P), grep("^ 0$", P))] <- "<.0001"
+    P[grep("0.000$", P)] <- "<.001"
     P.unadj <- ifelse(P.unadj < 1e-04, 0, P.unadj)
     P.unadj <- format(round(P.unadj, 4))
     diag(P.unadj) <- ""
-    P.unadj[c(grep("0.0000", P.unadj), grep("^ 0$", P.unadj))] <- "<.0001"
-    result <- list(R=R, P=P, P.unadj=P.unadj, type=type)
+    P.unadj[c(grep("0.0000$", P.unadj), grep("^ 0$", P.unadj))] <- "<.0001"
+    P.unadj[grep("0.000$", P.unadj)] <- "<.001"
+    result <- list(R = R, P = P, P.unadj = P.unadj, type = type)
     class(result) <- "rcorr.adjust"
     result
-}
+  }
 
 print.rcorr.adjust <- function(x, ...){
     cat("\n", if (x$type == "pearson") "Pearson" else "Spearman", "correlations:\n")
