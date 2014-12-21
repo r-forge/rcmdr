395aab91a435f6c363f834d51f99dca2089b15ed
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-12-21 18:11:15 +0000

    devel for Rcmdr 2.2-0. John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdr@606 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/Rcmdr-devel/R/ismdi.R
@@ -0,0 +1,7 @@
+# last modified 2014-04-06 by J. Fox
+
+# this function by Uwe Ligges; used with permission
+
+ismdi <- function(){
+    return(mdi = as.logical(.C("ismodemdi", as.integer(0), PACKAGE = "Rcmdr")[[1]]))
+}
