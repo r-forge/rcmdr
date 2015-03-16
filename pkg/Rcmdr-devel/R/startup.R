957bb445c4604ea5413f8ab683076a0145fc4456
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-03-16 19:38:12 +0000

    fix system path issue in OS X. John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdr@621 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Rcmdr-devel/R/startup.R
+++ pkg/Rcmdr-devel/R/startup.R
@@ -1,4 +1,4 @@
-# last modified 2014-12-20 by J. Fox
+# last modified 2015-03-16-12-20 by J. Fox
 #  applied patch to improve window behaviour supplied by Milan Bouchet-Valat 2011-09-22
 
 .onAttach <- function(...){
@@ -27,6 +27,18 @@
     if (!interactive()) return()
     save.options <- options(warn=-1)
     on.exit(options(save.options))
+    if (MacOSXP()){
+      PATH <- Sys.getenv("PATH")
+      putRcmdr("PATH", PATH)
+      PATH <- system2("/usr/libexec/path_helper", "-s", stdout=TRUE)
+      PATH <- sub("\"; export PATH;$", "", sub("^PATH=\\\"", "", PATH))
+      Sys.setenv(PATH=PATH)
+#       PATH <- unlist(strsplit(PATH, .Platform$path.sep, fixed=TRUE))
+#       if (length(grep("/usr/texbin", PATH)) == 0) {
+#         PATH[length(PATH) + 1] <- "/usr/texbin"
+#         Sys.setenv(PATH=paste(PATH, collapse=.Platform$path.sep))
+#       }
+    }
     required.packages <- rev(c("abind", "aplpack", "car", "colorspace", 
         "effects", "e1071", "foreign", "Hmisc", "knitr", "lattice", "leaps", "lmtest",
         "markdown", "MASS", "mgcv", "multcomp", "nlme", "nnet", "RcmdrMisc", "relimp", "rgl",
@@ -132,4 +144,11 @@
             tkwait.window(top)
         }
     }
+}
+
+.onUnload <- function(...){
+  if (MacOSXP()){
+    PATH <- getRcmdr("PATH", fail=FALSE)
+    if (!is.null(PATH)) Sys.setenv(PATH=PATH)
+  }
 }
