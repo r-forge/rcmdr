2d2ada3c9acb738bc242ad776e82f4318dd5f0d9
jfox (Professor Emeritus ) <jfox@mcmaster.ca>
2016-04-29 01:37:20 +0000

    version -> 1.0-5; added connect argument to plotMeans(). John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdr@716 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RcmdrMisc/R/plots.R
+++ pkg/RcmdrMisc/R/plots.R
@@ -1,6 +1,6 @@
 # various high-level plots
 
-# last modified 2014-09-04 by J. Fox
+# last modified 2016-04-28 by J. Fox
 
 Hist <- function(x, groups, scale=c("frequency", "percent", "density"), xlab=deparse(substitute(x)), 
     ylab=scale, main="", breaks="Sturges", ...){
@@ -130,7 +130,7 @@ plotDistr <- function(x, p, discrete=FALSE, cdf=FALSE, ...){
 plotMeans <- function(response, factor1, factor2, error.bars = c("se", "sd", "conf.int", "none"),
     level=0.95, xlab=deparse(substitute(factor1)), ylab=paste("mean of", deparse(substitute(response))),
     legend.lab=deparse(substitute(factor2)), main="Plot of Means",
-    pch=1:n.levs.2, lty=1:n.levs.2, col=palette(), ...){
+    pch=1:n.levs.2, lty=1:n.levs.2, col=palette(), connect=TRUE, ...){
     if (!is.numeric(response)) stop("Argument response must be numeric.")
     xlab # force evaluation
     ylab
@@ -151,7 +151,7 @@ plotMeans <- function(response, factor1, factor2, error.bars = c("se", "sd", "co
         levs <- levels(factor1)
         n.levs <- length(levs)
         plot(c(1, n.levs), yrange, type="n", xlab=xlab, ylab=ylab, axes=FALSE, main=main, ...)
-        points(1:n.levs, means, type="b", pch=16, cex=2)
+        points(1:n.levs, means, type=if (connect) "b" else "p", pch=16, cex=2)
         box()
         axis(2)
         axis(1, at=1:n.levs, labels=levs)
@@ -184,7 +184,7 @@ plotMeans <- function(response, factor1, factor2, error.bars = c("se", "sd", "co
         axis(2)
         axis(1, at=1:n.levs.1, labels=levs.1)
         for (i in 1:n.levs.2){
-            points(1:n.levs.1, means[, i], type="b", pch=pch[i], cex=2, col=col[i], lty=lty[i])
+            points(1:n.levs.1, means[, i], type=if (connect) "b" else "p", pch=pch[i], cex=2, col=col[i], lty=lty[i])
             if (error.bars != "none") arrows(1:n.levs.1, means[, i] - sds[, i],
                 1:n.levs.1, means[, i] + sds[, i], angle=90, code=3, col=col[i], lty=lty[i], length=0.125)
         }
