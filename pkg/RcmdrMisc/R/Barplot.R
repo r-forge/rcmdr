685955ff46e9c315f1112d2bc2bccb62db760b8c
jfox (Professor Emeritus ) <jfox@mcmaster.ca>
2016-09-21 02:10:12 +0000

    improved legends in Barplot(). John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdr@771 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RcmdrMisc/R/Barplot.R
+++ pkg/RcmdrMisc/R/Barplot.R
@@ -3,26 +3,55 @@ Barplot <- function(x, by, scale=c("frequency", "percent"),
                     style=c("divided", "parallel"),
                     col=if (missing(by)) "gray" else rainbow_hcl(length(levels(by))),
                     xlab=deparse(substitute(x)), 
-                    legend.title=deparse(substitute(by)), ylab=scale,
-                    legend.pos="topright", ...){
+                    legend.title=deparse(substitute(by)), ylab=scale, main=NULL,
+                    legend.pos="above", ...){
+    find.legend.columns <- function(n, target=min(4, n)){
+        rem <- n %% target
+        if (rem != 0 && rem < target/2) target <- target - 1
+        target
+    }
     if (!is.factor(x)) stop("x must be a factor")
     if (!missing(by) && !is.factor(by)) stop("by must be a factor")
     scale <- match.arg(scale)
     style <- match.arg(style)
+    if (legend.pos == "above"){
+        mar <- par("mar")
+        mar[3] <- mar[3] + 2
+        old.mar <- par(mar=mar)
+        on.exit(par(old.mar))
+    }
     if (missing(by)){
         y <- table(x)
         if (scale == "percent") y <- 100*y/sum(y)
-        barplot(y, xlab=xlab, ylab=ylab, col=col, ...)
+        barplot(y, xlab=xlab, ylab=ylab, col=col, main=main, ...)
     }
     else{
-        col <- col[1:length(levels(by))]
+        nlevels <- length(levels(by))
+        col <- col[1:nlevels]
         y <- table(by, x)
         if (scale == "percent") {
             y <- if (conditional) 100*apply(y, 2, function(x) x/sum(x))
                  else 100*y/sum(y)
         }
-        barplot(y, xlab=xlab, ylab=ylab, legend.text=levels(by),
-                col=col, 
+        if (legend.pos == "above"){
+            legend.columns <- find.legend.columns(nlevels)
+            top <- 4 + ceiling(nlevels/legend.columns)
+            xpd <- par(xpd=TRUE)
+            on.exit(par(xpd=xpd), add=TRUE)
+            barplot(y, xlab=xlab, ylab=ylab,
+                    col=col, 
+                    beside = style == "parallel", ...)
+            usr <- par("usr")
+            legend.x <- usr[1]
+            legend.y <- usr[4] + 1.2*top*strheight("x")
+            legend.pos <- list(x=legend.x, y=legend.y)
+            title(main=main, line=mar[3] - 1)
+            legend(legend.pos, title=legend.title, legend=levels(by), 
+                   fill=col,
+                   ncol=legend.columns, inset=0.05)
+        }
+        else barplot(y, xlab=xlab, ylab=ylab, main=main,
+                legend.text=levels(by), col=col, 
                 args.legend=list(x=legend.pos, title=legend.title, inset=0.05),
                 beside = style == "parallel", ...)
     }
