60b596b293fceb1c772e2ecc4541a909f17be165
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-02-06 23:58:33 +0000

    added plotBoot(). John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdr@817 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/RcmdrMisc/R/plotBoot.R
@@ -0,0 +1,28 @@
+plotBoot <- function(object, confint=NULL, ...){
+  UseMethod("plotBoot")
+}
+
+plotBoot.boot <- function(object, confint=NULL, ...){
+  mfrow <- function (n) {
+    rows <- round(sqrt(n))
+    cols <- ceiling(n/rows)
+    c(rows, cols)
+  }
+  if (is.null(confint)) confint <- confint(object)
+  t0 <- object$t0
+  t <- object$t
+  if (any(is.na(t))){
+    t <- na.omit(t)
+    message("Note: bootstrap samples with missing parameter values suppressed")
+  }
+  npars <- length(t0)
+  pars <- names(t0)
+  savepar <- par(mfrow=mfrow(npars), oma=c(0, 0, 2, 0), mar=c(5.1, 4.1, 2.1, 2.1))
+  on.exit(par(savepar))
+  for (i in 1:npars){
+    car::densityPlot(t[, i], xlab=pars[i], method="adaptive")
+    abline(v=t0[i], lty=2, col="blue")
+    abline(v=confint[i, ], lty=2, col="magenta")
+  }
+  title(main="Bootstrap Distributions", outer=TRUE, line=0.5)
+}
