8d907089ff5e8f58c1e4d361ed50d7f983f2c5fe
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-04-05 20:39:20 +0000

    improvements to binnedCounts(). John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdr@828 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RcmdrMisc/R/binnedCounts.R
+++ pkg/RcmdrMisc/R/binnedCounts.R
@@ -1,7 +1,18 @@
-binnedCounts <- function(x, breaks="Sturges"){
+binnedCounts <- function(x, breaks="Sturges", name=deparse(substitute(x))){
+    if (is.data.frame(x)) x <- as.matrix(x)
+    if (is.matrix(x)) {
+        names <- colnames(x)
+        for (j in 1:ncol(x)){
+            binnedCounts(x[, j], breaks=breaks, name=names[j])
+            cat("\n")
+        }
+        return(invisible(NULL))
+    }
     dist <- hist(x, breaks=breaks, plot=FALSE)
     counts <- dist$counts
     breaks <- dist$breaks
     names(counts) <- paste0(breaks[1:(length(breaks) - 1)], ":", breaks[-1])
-    counts
+    cat("distribution of", name, "\n")
+    print(counts)
+    return(invisible(counts))
 }
