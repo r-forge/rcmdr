8d9026272cb80337086747086b168df36fe7167e
jfox (Professor Emeritus ) <jfox@mcmaster.ca>
2018-07-14 23:22:37 +0000

    shows ticks only at observed values in discretePlot()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdr@871 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RcmdrMisc/R/discretePlot.R
+++ pkg/RcmdrMisc/R/discretePlot.R
@@ -5,7 +5,10 @@ discretePlot <- function(x, by, scale=c("frequency", "percent"), xlab=deparse(su
         y <- as.vector(table(x))
         if (scale == "percent") y <- 100*y/sum(y)
         x <- sort(unique(x))
-        plot(x, y, type="h", xlab=xlab, ylab=ylab, main=main, xlim=xlim, ylim=ylim)
+        plot(x, y, type="h", xlab=xlab, ylab=ylab, main=main, xlim=xlim, ylim=ylim,
+             axes=FALSE, frame.plot=TRUE)
+        axis(2)
+        axis(1, at=x)
         points(x, y, pch=16)
         abline(h=0, col="gray")
     }
