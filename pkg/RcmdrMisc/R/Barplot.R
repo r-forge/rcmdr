1ab530cce52dce72b938132c82a13b42e1b9815b
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-11-21 23:19:35 +0000

    add counts/percents to bars in Barplot()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdr@955 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RcmdrMisc/R/Barplot.R
+++ pkg/RcmdrMisc/R/Barplot.R
@@ -4,7 +4,7 @@ Barplot <- function(x, by, scale=c("frequency", "percent"),
                     col=if (missing(by)) "gray" else rainbow_hcl(length(levels(by))),
                     xlab=deparse(substitute(x)), 
                     legend.title=deparse(substitute(by)), ylab=scale, main=NULL,
-                    legend.pos="above", ...){
+                    legend.pos="above", label.bars=TRUE, ...){
     find.legend.columns <- function(n, target=min(4, n)){
         rem <- n %% target
         if (rem != 0 && rem < target/2) target <- target - 1
@@ -23,7 +23,11 @@ Barplot <- function(x, by, scale=c("frequency", "percent"),
     if (missing(by)){
         y <- table(x)
         if (scale == "percent") y <- 100*y/sum(y)
-        barplot(y, xlab=xlab, ylab=ylab, col=col, main=main, ...)
+        mids <- barplot(y, xlab=xlab, ylab=ylab, col=col, main=main, ...)
+        if(label.bars){
+            labels <- if (scale == "percent") paste(round(y), "%") else y
+            text(mids, y, labels, pos=1, offset=0.5)
+        }
     }
     else{
         nlevels <- length(levels(by))
@@ -31,16 +35,16 @@ Barplot <- function(x, by, scale=c("frequency", "percent"),
         y <- table(by, x)
         if (scale == "percent") {
             y <- if (conditional) 100*apply(y, 2, function(x) x/sum(x))
-                 else 100*y/sum(y)
+            else 100*y/sum(y)
         }
         if (legend.pos == "above"){
             legend.columns <- find.legend.columns(nlevels)
             top <- 4 + ceiling(nlevels/legend.columns)
             xpd <- par(xpd=TRUE)
             on.exit(par(xpd=xpd), add=TRUE)
-            barplot(y, xlab=xlab, ylab=ylab,
-                    col=col, 
-                    beside = style == "parallel", ...)
+            mids <- barplot(y, xlab=xlab, ylab=ylab,
+                            col=col, 
+                            beside = style == "parallel", ...)
             usr <- par("usr")
             legend.x <- usr[1]
             legend.y <- usr[4] + 1.2*top*strheight("x")
@@ -50,10 +54,16 @@ Barplot <- function(x, by, scale=c("frequency", "percent"),
                    fill=col,
                    ncol=legend.columns, inset=0.05)
         }
-        else barplot(y, xlab=xlab, ylab=ylab, main=main,
-                legend.text=levels(by), col=col, 
-                args.legend=list(x=legend.pos, title=legend.title, inset=0.05, bg="white"),
-                beside = style == "parallel", ...)
+        else mids <- barplot(y, xlab=xlab, ylab=ylab, main=main,
+                             legend.text=levels(by), col=col, 
+                             args.legend=list(x=legend.pos, title=legend.title, inset=0.05, bg="white"),
+                             beside = style == "parallel", ...)
+        if (label.bars){
+            yy <- if (is.matrix(mids)) as.vector(y) else as.vector(apply(y, 2, cumsum))
+            labels <- if (scale == "percent") paste(round(as.vector(y)), "%") else as.vector(y)
+            xx <- if (is.vector(mids)) rep(mids, each=ncol(y)) else as.vector(mids)
+            text(xx, yy, labels, pos=1, offset=0.5)
+        }
     }
-    return(invisible(NULL))
+    return(invisible(mids))
 }
