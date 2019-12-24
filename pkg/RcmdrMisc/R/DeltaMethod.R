48d6e9fc1dfa5eb18fc06a1a917daa8eb3ff2bc0
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-12-24 15:39:31 +0000

    fix level arg in DeltaMethod()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdr@972 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RcmdrMisc/R/DeltaMethod.R
+++ pkg/RcmdrMisc/R/DeltaMethod.R
@@ -1,10 +1,10 @@
-# last modified 2016-04-01
+# last modified 2019-12-24
 
 DeltaMethod <- function(model, g, level=0.95){
   coefs <- coef(model)
   p <- length(coefs)
   nms <- if (names(coefs)[1] == "(Intercept)") paste0("b", 0:(p - 1)) else paste0("b", 1:p)
-  res <- car::deltaMethod(model, g, parameterNames=nms)
+  res <- car::deltaMethod(model, g, level=level, parameterNames=nms)
   result <- list(test=res, coef=rbind(names(coefs), nms))
   class(result) <- "DeltaMethod"
   result
