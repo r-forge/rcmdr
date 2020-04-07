f58201dbf13d89340db412da89b011947bb91992
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2020-04-07 16:02:25 +0000

    use within-subjects factors for trace and xvar in repeatedMeasuresPlot()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdr@989 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RcmdrMisc/R/repeatedMeasuresPlot.R
+++ pkg/RcmdrMisc/R/repeatedMeasuresPlot.R
@@ -1,4 +1,4 @@
-# last modified: 2020-04-06
+# last modified: 2020-04-07
 
 repeatedMeasuresPlot <- function(data, within, within.names, within.levels, between.names=NULL,
                                  response.name="score", trace, xvar, col=palette()[-1]){
@@ -49,31 +49,78 @@ repeatedMeasuresPlot <- function(data, within, within.names, within.levels, betw
     means
   }
   
-  rmPlot <- function(data){
-    n.levels <- sapply(data[, - ncol(data), drop=FALSE], function(x) length(levels(x)))
+  rmPlot <- function(data) {
+    n.levels <-
+      sapply(data[,-ncol(data), drop = FALSE], function(x)
+        length(levels(x)))
     n.factors <- length(n.levels)
-    fnames <- names(data)[- ncol(data), drop=FALSE]
-    trace <- if (is.na(trace)) which.min(n.levels) else which(fnames == trace)
-    xvar <- if (is.na(xvar)) which.max(n.levels) else which(fnames == xvar)
-    if (n.factors == 1) trace <- NULL
-    if (!is.null(trace) && trace == xvar) stop("trace and xvar factors are identical: ", fnames[trace])
-    form <- paste(response.name, " ~", fnames[xvar], 
-                  if (n.factors > 2) "|", paste(fnames[-c(trace, xvar)], collapse="+"))
-    tr.levels <- n.levels[trace]
-    if(!is.null(trace)){
-      xyplot(as.formula(form),
-             groups = if (!is.null(trace)) data[[trace]] else 1, 
-             type="b", lty=1:tr.levels, pch=1:tr.levels, col=col[1:tr.levels], cex=1.25,
-             strip=function(...) strip.default(strip.names=c(TRUE, TRUE), ...),
-             data=data,
-             ylab=paste("mean", response.name),
-             key=if (!is.null(trace)) list(title=fnames[trace], cex.title=1,
-                      text=list(levels(data[[trace]])),
-                      lines=list(lty=1:tr.levels, col=col[1:tr.levels]),
-                      points=list(pch=1:tr.levels, col=col[1:tr.levels], cex=1.25))
-             )
+    fnames <- names(data)[-ncol(data), drop = FALSE]
+    trace <- if (is.na(trace)) {
+      wnames <- if (!is.na(xvar)) within.names[!(fnames == xvar)] else within.names
+      if (length(within.names) == 1 && n.factors > 1) {
+        which.min(n.levels[!(fnames == within.names)])
+      } else {
+        if (length(wnames) > 0) which.min(n.levels[wnames]) else NULL
+      }
     } else {
-      xyplot(as.formula(form), type="b", lty=1, pch=1, col=col[1], cex=1,25, data=data)
+      which(fnames == trace)
+    }
+    xvar <- if (is.na(xvar)) {
+      wnames <- if (!is.na(trace)) within.names[!(fnames == trace)] else within.names
+      which.max(n.levels[wnames])
+    } else {
+      which(fnames == xvar)
+    }
+    if (n.factors == 1 || trace == xvar) trace <- NULL
+    form <- paste(response.name,
+                  " ~",
+                  fnames[xvar],
+                  if (n.factors > 2)
+                    "|",
+                  paste(fnames[-c(trace, xvar)], collapse = "+"))
+    tr.levels <- n.levels[trace]
+    if (!is.null(trace)) {
+      xyplot(
+        as.formula(form),
+        groups = if (!is.null(trace))
+          data[[trace]]
+        else
+          1,
+        type = "b",
+        lty = 1:tr.levels,
+        pch = 1:tr.levels,
+        col = col[1:tr.levels],
+        cex = 1.25,
+        strip = function(...)
+          strip.default(strip.names = c(TRUE, TRUE), ...),
+        data = data,
+        ylab = paste("mean", response.name),
+        key = if (!is.null(trace))
+          list(
+            title = fnames[trace],
+            cex.title = 1,
+            text = list(levels(data[[trace]])),
+            lines = list(lty = 1:tr.levels, col = col[1:tr.levels]),
+            points = list(
+              pch = 1:tr.levels,
+              col = col[1:tr.levels],
+              cex = 1.25
+            )
+          )
+      )
+    } else {
+      xyplot(
+        as.formula(form),
+        type = "b",
+        lty = 1,
+        pch = 1,
+        col = col[1],
+        cex = 1.25,
+        strip = function(...)
+          strip.default(strip.names = c(TRUE, TRUE), ...),
+        data = data,
+        ylab = paste("mean", response.name)
+      )
     }
   }
   
