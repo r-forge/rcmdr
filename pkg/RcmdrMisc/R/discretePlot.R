9820903be22b04d0a97394b707db031f3453fe98
jfox (Professor Emeritus ) <jfox@mcmaster.ca>
2020-09-17 18:11:41 +0000

    version -> 2.7-2; xlim, ylim args for discretePlot()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdr@1042 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RcmdrMisc/R/discretePlot.R
+++ pkg/RcmdrMisc/R/discretePlot.R
@@ -1,19 +1,23 @@
-discretePlot <- function(x, by, scale=c("frequency", "percent"), xlab=deparse(substitute(x)), ylab=scale, main=""){
+discretePlot <- function(x, by, scale=c("frequency", "percent"), xlab=deparse(substitute(x)), ylab=scale, main="", 
+                         xlim=NULL, ylim=NULL, ...){
     force(xlab)
     scale <- match.arg(scale)
-    dp <- function(x, scale, xlab, ylab, main, xlim=range(x), ylim=c(0, max(y))){
+    dp <- function(x, scale, xlab, ylab, main, xlim, ylim){
         y <- as.vector(table(x))
         if (scale == "percent") y <- 100*y/sum(y)
         x <- sort(unique(x))
-        plot(x, y, type="h", xlab=xlab, ylab=ylab, main=main, xlim=xlim, ylim=ylim,
-             axes=FALSE, frame.plot=TRUE)
+        if (is.null(ylim)) ylim <- c(0, max(y, na.rm=TRUE))
+        plot(x, y, type=if (min(ylim) == 0) "h" else "n", xlab=xlab, ylab=ylab, main=main, xlim=xlim, ylim=ylim,
+             axes=FALSE, frame.plot=TRUE, ...)
         axis(2)
         axis(1, at=x)
         points(x, y, pch=16)
         abline(h=0, col="gray")
     }
+    if (is.null(xlim)) xlim <- range(x, na.rm=TRUE)
+    if (!is.null(ylim) && min(ylim) != 0) warning("the lower end of the y-axis is not 0")
     if (missing(by)){
-        dp(na.omit(x), scale, xlab, ylab, main)
+        dp(na.omit(x), scale, xlab, ylab, main, xlim, ylim, ...)
     }
     else{
         by.var <- deparse(substitute(by))
@@ -26,18 +30,20 @@ discretePlot <- function(x, by, scale=c("frequency", "percent"), xlab=deparse(su
         complete <- complete.cases(x, by)
         x <- x[complete]
         by <- by[complete]
-        max.y <- if (scale == "frequency") max(table(x, by))
-            else {
-                tab <- colPercents(table(x, by))
-                max(tab[1:(nrow(tab) - 2), ])
-            }
-        xlim  <- range(x)
+        if (is.null(ylim)){
+            max.y <- if (scale == "frequency") max(table(x, by))
+                else {
+                    tab <- colPercents(table(x, by))
+                    max(tab[1:(nrow(tab) - 2), ])
+                }
+            ylim <- c(0, max.y)
+        }
         levels <- levels(by)
         save.par <- par(mfcol=c(length(levels), 1))
         on.exit(par(save.par))
         for (level in levels){
             dp(x[by == level], scale=scale, xlab=xlab, ylab=ylab, main = paste(by.var, "=", level), 
-               xlim=xlim, ylim=c(0, max.y))
+               xlim=xlim, ylim=ylim, ...)
         }
     }
 }
